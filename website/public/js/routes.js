078e17628d7045a79327df4402cd606b14b705ba
sameesiddiqui <sameezsiddiqui@gmail.com>
2017-05-31 20:03:51 -0400

    added first route to map

---
+++ website/public/js/routes.js
@@ -0,0 +1,26 @@
+function initMap() {
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    center: {lat: 38.83, lng: -77.16}
+  });
+  directionsDisplay.setMap(map);
+
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  directionsService.route({
+    origin: "Burke Centre Amtrak Station",
+    destination: "Farragut North Station",
+    travelMode: 'DRIVING'
+  }, function(response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
