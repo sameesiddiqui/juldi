80c50c49b2c08daa603ac1119385b21c6ee96ece
sameesiddiqui <sameezsiddiqui@gmail.com>
2017-06-03 19:49:30 -0400

    updated example secret js file

--- website/example_secret.js
+++ website/example_secret.js
@@ -1,5 +1,5 @@
-//This is a sample secret.js file
-//this contains credentials / other sensitive information
+// This is a sample secret.js file
+// this contains credentials / other sensitive information
 
 exports.dbinfo = {
   host: 'localhost',
@@ -10,4 +10,15 @@ exports.dbinfo = {
 exports.emailinfo = {
   user: 'cat@dog.com',
   pass: 'securepassword'
+}
+
+exports.stripekeys = {
+  pk_test: '',
+  sk_test: '',
+  public_key: '',
+  secret_key: ''
+}
+
+exports.googlekeys = {
+  maps: ''
 }
