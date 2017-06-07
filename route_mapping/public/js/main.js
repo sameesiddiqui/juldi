fff0a1bc475dfabf2fdfbf75f33ff6d2be5b2de3
sameesiddiqui <sameezsiddiqui@gmail.com>
2017-06-07 18:02:06 -0400

    map clustering

--- route_mapping/public/js/main.js
+++ route_mapping/public/js/main.js
@@ -1,474 +1,69 @@
 function initMap() {
+
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
-    center: {lat: 38.8315541, lng: -77.3120885}
+    zoom: 10,
+   	center: {lat: 38.83, lng: -77.16}
   });
-  var geocoder = new google.maps.Geocoder();
 
-  // var imported = document.createElement('script');
-  // imported.src = 'data.json';
-  // document.head.appendChild(imported);
+  // Create an array of alphabetical characters used to label the markers.
+  var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`-=[];:.,/?><+_';
 
-  // $.getScript('data.json', function(){
-    // var addresses = JSON.parse(data)
-    //   console.log(addresses)
-
-      setTimeout(function() {
-        for (var i=0; i < data.addresses.length; i++) {
-          var address = data.addresses[i]
-          geocodeAddress(geocoder, map, address.start)
-          console.log(address)
-        }
-      }, 5000)
-
-  // })
-
-  document.getElementById('submit').addEventListener('click', function() {
-    geocodeAddress(geocoder, map, null);
+  // Add some markers to the map.
+  // Note: The code uses the JavaScript Array.prototype.map() method to
+  // create an array of markers based on a given "locations" array.
+  // The map() method here has nothing to do with the Google Maps API.
+  var markers = starting.map(function(location, i) {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length]
+    });
   });
+
+  // Add a marker clusterer to manage the markers.
+  var markerCluster = new MarkerClusterer(map, markers,
+      {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 }
-
-function geocodeAddress(geocoder, resultsMap, address) {
-  if (address == null) {
-      address = document.getElementById('address').value;
-  }
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === 'OK') {
-      resultsMap.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
-
-var data = {
-"addresses": [
-    {
-        "id": 4,
-        "email": "ruparagu@gmail.com",
-        "start": "Gainesville, VA, United States",
-        "dest": "L Street Northwest, Washington, DC, United States",
-        "timehrs": "08",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 5,
-        "email": "hirenbijal9@gmail.com",
-        "start": "12919 Alton Square, Herndon, VA, United States",
-        "dest": "1001 G Street Northwest, Washington, DC, United States",
-        "timehrs": "09",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 8,
-        "email": "suparna.ray@gmail.com",
-        "start": "Herndon-Monroe Park and Ride, Sunrise Valley Drive, Herndon, VA, United States",
-        "dest": "Smithsonian Metro Station, Washington, DC, United States",
-        "timehrs": "09",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 9,
-        "email": "hussein_h_ali@hotmail.com",
-        "start": "Herndon-Monroe Park and Ride, Sunrise Valley Drive, Herndon, VA, United States",
-        "dest": "Farragut West, 18th Street Northwest, Washington, DC, United States",
-        "timehrs": "09",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 10,
-        "email": "divieshn@gmail.com",
-        "start": "Herndon-Monroe Park and Ride, Sunrise Valley Drive, Herndon, VA, United States",
-        "dest": "The World Bank, 19th Street Northwest, Washington, DC, United States",
-        "timehrs": "09",
-        "timemins": "30",
-        "timeampm": "AM"
-    },
-    {
-        "id": 11,
-        "email": "k_sobhan@yahoo.com",
-        "start": "13800 Jefferson Park Drive, Herndon, VA, United States",
-        "dest": "Farragut West, 18th Street Northwest, Washington, DC, United States",
-        "timehrs": "09",
-        "timemins": "30",
-        "timeampm": "AM"
-    },
-    {
-        "id": 13,
-        "email": "RWoodbury07@gmail.com",
-        "start": "Herndon-Monroe Park and Ride, Sunrise Valley Drive, Herndon, VA, United States",
-        "dest": "L'Enfant Plaza, L'Enfant Plaza Southwest, Washington, DC, United States",
-        "timehrs": "09",
-        "timemins": "15",
-        "timeampm": "AM"
-    },
-    {
-        "id": 19,
-        "email": "lija.varkey@gmail.com",
-        "start": "Emerald Chase Drive, Herndon, VA, United States",
-        "dest": "Crystal Drive, Arlington, VA, United States",
-        "timehrs": "09",
-        "timemins": "30",
-        "timeampm": "AM"
-    },
-    {
-        "id": 20,
-        "email": "naortiz89@gmail.com",
-        "start": "14533 Picket Oaks Road, Centreville, VA, United States",
-        "dest": "1211 Connecticut Avenue Northwest, Washington, DC, United States",
-        "timehrs": "08",
-        "timemins": "15",
-        "timeampm": "AM"
-    },
-    {
-        "id": 21,
-        "email": "yadavss@gmail.com",
-        "start": "3138 Borge Street, Oakton, VA, United States",
-        "dest": "6359 Walker Lane, Franconia, VA, United States",
-        "timehrs": "08",
-        "timemins": "30",
-        "timeampm": "AM"
-    },
-    {
-        "id": 22,
-        "email": "smxsid@yahoo.com",
-        "start": "3114 Spring Drive, Alexandria, VA, United States",
-        "dest": "George Mason University, University Drive, Fairfax, VA, United States",
-        "timehrs": "10",
-        "timemins": "05",
-        "timeampm": "AM"
-    },
-    {
-        "id": 23,
-        "email": "",
-        "start": "9451 Lee Highway, Fairfax, VA, United States",
-        "dest": "14721 Industry Court Woodbridge, VA 22191",
-        "timehrs": "12",
-        "timemins": "50",
-        "timeampm": "PM"
-    },
-    {
-        "id": 24,
-        "email": "reema2001@gmail.com",
-        "start": "9730 Cheriton Court, Vienna, VA, United States",
-        "dest": "7389 Lee Highway Service Road, Falls Church, VA, United States",
-        "timehrs": "08",
-        "timemins": "45",
-        "timeampm": "AM"
-    },
-    {
-        "id": 25,
-        "email": "reema2001@gmail.com",
-        "start": "7389 Lee Highway Service Road, Falls Church, VA, United States",
-        "dest": "9730 Cheriton Court, Vienna, VA, United States",
-        "timehrs": "06",
-        "timemins": "00",
-        "timeampm": "PM"
-    },
-    {
-        "id": 26,
-        "email": "hwajungk@gmail.com",
-        "start": "403 Dove Circle Southwest, Vienna, VA, United States",
-        "dest": "441 4th Street Northwest, Washington, DC, United States",
-        "timehrs": "08",
-        "timemins": "50",
-        "timeampm": "AM"
-    },
-    {
-        "id": 27,
-        "email": "tuluucamtran@gmail.com",
-        "start": "Centreville Park & Ride, Early Autumn Drive, Centreville, VA, United States",
-        "dest": "1100 4th Street Southwest, Washington, DC, United States",
-        "timehrs": "08",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 28,
-        "email": "",
-        "start": "George Mason University - Fairfax Campus 4400 University Dr Fairfax, VA  22030 United States",
-        "dest": "1633 Wisconsin Ave NW Washington, DC  20007 United States",
-        "timehrs": "10",
-        "timemins": "30",
-        "timeampm": "AM"
-    },
-    {
-        "id": 29,
-        "email": "mgottshalk@gmail.com",
-        "start": "3 Exeter Court, Sterling, VA, United States",
-        "dest": "475 L'Enfant Plaza Southwest, Washington, DC, United States",
-        "timehrs": "07",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 30,
-        "email": "cody.talmadge@gmail.com",
-        "start": "425 L St NW, Washington, DC, United States",
-        "dest": "901 D St NW, Washington, DC, United States",
-        "timehrs": "09",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 31,
-        "email": "lmberry1@gmail.com",
-        "start": "3933 Wilcoxson Drive, Fairfax, VA, United States",
-        "dest": "Farragut West, 18th Street Northwest, Washington, DC, United States",
-        "timehrs": "09",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 32,
-        "email": "yazakout@yahoo.com",
-        "start": "7206 Regent Drive, Alexandria, VA, United States",
-        "dest": "13800 Air and Space Museum Parkway, Herndon, VA, United States",
-        "timehrs": "09",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 34,
-        "email": "jacqueline.davison@gmail.com",
-        "start": "2117 Arlington Terrace, Alexandria, VA, United States",
-        "dest": "5107 Leesburg Pike, Falls Church, VA, United States",
-        "timehrs": "07",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 35,
-        "email": "alex.catanese@gmail.com",
-        "start": "1428 12th Street North, Arlington, VA, United States",
-        "dest": "1146 19th Street Northwest, Washington, DC, United States",
-        "timehrs": "08",
-        "timemins": "45",
-        "timeampm": "AM"
-    },
-    {
-        "id": 36,
-        "email": "gracemtran@gmail.com",
-        "start": "53 Skyhill Rd, Alexandria, VA, United States",
-        "dest": "Metro Center Station, 13th Street Northwest, Washington, DC, United States",
-        "timehrs": "09",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 37,
-        "email": "angelkollie1497@gmail.com",
-        "start": "George Mason University, University Drive, Fairfax, VA, United States",
-        "dest": "Centreville, VA, United States",
-        "timehrs": "03",
-        "timemins": "00",
-        "timeampm": "PM"
-    },
-    {
-        "id": 38,
-        "email": "lee2cx@gmail.com",
-        "start": "14611 Battery Ridge Lane, Centreville, VA, United States",
-        "dest": "875 North Randolph Street, Arlington, VA, United States",
-        "timehrs": "08",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 39,
-        "email": "dtekkali@gmail.com",
-        "start": "13724 Neil Armstrong Avenue, Herndon, VA, United States",
-        "dest": "Washington, DC, United States",
-        "timehrs": "07",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 40,
-        "email": "dll201@verizon.net",
-        "start": "Fair Oaks Mall, Fairfax, VA, United States",
-        "dest": "7 th and D street Washington DC",
-        "timehrs": "07",
-        "timemins": "45",
-        "timeampm": "AM"
-    },
-    {
-        "id": 41,
-        "email": "shannon.charland@gmail.com",
-        "start": "11019 Becontree Lake Drive, Reston, VA, United States",
-        "dest": "20 Massachusetts Avenue Northwest, Washington, DC, United States",
-        "timehrs": "08",
-        "timemins": "45",
-        "timeampm": "AM"
-    },
-    {
-        "id": 42,
-        "email": "",
-        "start": "Bristow, VA, United States",
-        "dest": "George Mason University - Science and Technology Campus, University Boulevard, Manassas, VA, United States",
-        "timehrs": "08",
-        "timemins": "30",
-        "timeampm": "AM"
-    },
-    {
-        "id": 43,
-        "email": "barelyahistorian@gmail.com",
-        "start": "45518 Cambers Trail Terrace, Sterling, VA, United States",
-        "dest": "700 Pennsylvania Avenue Northwest, Washington, DC, United States",
-        "timehrs": "07",
-        "timemins": "30",
-        "timeampm": "AM"
-    },
-    {
-        "id": 44,
-        "email": "mosehurt@gmail.com",
-        "start": "Ashburn, VA, United States",
-        "dest": "441 G Street Northwest, Washington, DC, United States",
-        "timehrs": "07",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 45,
-        "email": "burkej105@msn.con",
-        "start": "Ashburn, VA, United States",
-        "dest": "425 3rd Street Southwest, Washington, DC, United States",
-        "timehrs": "07",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 46,
-        "email": "",
-        "start": "Loudoun Station Drive, Ashburn, VA, United States",
-        "dest": "Eisenhower Avenue, Alexandria, VA, United States",
-        "timehrs": "08",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 47,
-        "email": "Kimberlee3s@icloud.com",
-        "start": "9893 Airedale Court, Bristow, VA, United States",
-        "dest": "701 Pennsylvania Avenue Northwest, Washington, DC, United States",
-        "timehrs": "08",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 48,
-        "email": "m_vaughter@hotmail.com",
-        "start": "43401 Southland Street, Ashburn, VA, United States",
-        "dest": "111 S George Mason Dr, Arlington, VA, United States",
-        "timehrs": "08",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 49,
-        "email": "californiabound06@yahoo.com",
-        "start": "12817 Victory Lakes Loop, Bristow, VA, United States",
-        "dest": "1145 17th Street Northwest, Washington, DC, United States",
-        "timehrs": "07",
-        "timemins": "30",
-        "timeampm": "AM"
-    },
-    {
-        "id": 50,
-        "email": "talk2thehobbit@gmail.com",
-        "start": "2975 Hunters Branch Road, Fairfax, VA, United States",
-        "dest": "2401 E Street Northwest, Washington, DC, United States",
-        "timehrs": "08",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 51,
-        "email": "zakirhos@yahoo.com",
-        "start": "4920 Stringfellow Road, Centreville, VA, United States",
-        "dest": "200 Constitution Avenue Northwest, Washington, DC, United States",
-        "timehrs": "09",
-        "timemins": "30",
-        "timeampm": "AM"
-    },
-    {
-        "id": 52,
-        "email": "ramonpablosoto@gmail.com",
-        "start": "44028 Riverpoint Drive, Leesburg, VA, United States",
-        "dest": "4301 N Fairfax Dr, Arlington, VA, United States",
-        "timehrs": "08",
-        "timemins": "30",
-        "timeampm": "AM"
-    },
-    {
-        "id": 53,
-        "email": "totally.owned.u@gmail.com",
-        "start": "7501 Evans Ford Road, Clifton, VA, United States",
-        "dest": "Infinite Loop, Cupertino, CA, United States",
-        "timehrs": "09",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 54,
-        "email": "cuinhevn@verizon.net",
-        "start": "4023 Rosemeade Drive, Fairfax, VA, United States",
-        "dest": "L'Enfant Plaza Southwest, Washington, DC, United States",
-        "timehrs": "07",
-        "timemins": "30",
-        "timeampm": "AM"
-    },
-    {
-        "id": 55,
-        "email": "pham@2001clarendonapts.com",
-        "start": "13806 Braddock Springs Road, Centreville, VA, United States",
-        "dest": "2001 Clarendon Blvd, Arlington, VA, United States",
-        "timehrs": "08",
-        "timemins": "00",
-        "timeampm": "AM"
-    },
-    {
-        "id": 56,
-        "email": "terrysky83@gmail.com",
-        "start": "13974 Khalid Lane, Chantilly, VA, United States",
-        "dest": "600 E St NW, Washington, DC, United States",
-        "timehrs": "08",
-        "timemins": "10",
-        "timeampm": "AM"
-    },
-    {
-        "id": 57,
-        "email": "lucymuro20@gmail.com",
-        "start": "2994 Rittenhouse Circle, Fairfax, VA, United States",
-        "dest": "1120 Vermont Avenue Northwest, Washington, DC, United States",
-        "timehrs": "08",
-        "timemins": "30",
-        "timeampm": "AM"
-    },
-    {
-        "id": 58,
-        "email": "735465@gmail.com",
-        "start": "4457 Oakdale Crescent Ct, Fairfax, VA, United States",
-        "dest": "1776 Wilson Boulevard, Arlington, VA, United States",
-        "timehrs": "08",
-        "timemins": "30",
-        "timeampm": "AM"
-    },
-    {
-        "id": 59,
-        "email": "ned.malik@gmail.com",
-        "start": "12100 Vale Road, Oakton, VA, United States",
-        "dest": "120 S Street Northwest, Washington, DC, United States",
-        "timehrs": "09",
-        "timemins": "00",
-        "timeampm": "AM"
-    }
+var starting = [
+  {lat: 38.7956, lng: -77.6138},
+  {lat: 38.9557, lng: -77.3944},
+  {lat: 38.9510, lng: -77.3840},
+  {lat: 38.9510, lng: -77.3840},
+  {lat: 38.9510, lng: -77.3840},
+  {lat: 38.9502, lng: -77.4245},
+  {lat: 38.9510, lng: -77.3840},
+  {lat: 38.9207, lng: -77.4022},
+  {lat: 38.8252, lng: -77.4525},
+  {lat: 38.8720, lng: -77.3021},
+  {lat: 38.7652, lng: -77.0856},
+  {lat: 38.8875, lng: -77.2820},
+  {lat: 38.8752, lng: -77.2123},
+  {lat: 38.8851, lng: -77.2554},
+  {lat: 38.8359, lng: -77.4552},
+  {lat: 39.0464, lng: -77.4127},
+  {lat: 38.9039, lng: -77.0173},
+  {lat: 38.8445, lng: -77.2792},
+  {lat: 38.7605, lng: -77.0519},
+  {lat: 38.7945, lng: -77.0677},
+  {lat: 38.8879, lng: -77.0738},
+  {lat: 38.8086, lng: -77.0766},
+  {lat: 38.8315, lng: -77.3120},
+  {lat: 38.8449, lng: -77.4515},
+  {lat: 38.9210, lng: -77.4218},
+  {lat: 38.8627, lng: -77.3592},
+  {lat: 38.9647, lng: -77.3253},
+  {lat: 38.9992, lng: -77.4258},
+  {lat: 39.0437, lng: -77.4874},
+  {lat: 39.0437, lng: -77.4874},
+  {lat: 38.7429, lng: -77.5740},
+  {lat: 38.9961, lng: -77.5004},
+  {lat: 38.7599, lng: -77.5700},
+  {lat: 38.8739, lng: -77.2662},
+  {lat: 38.8541, lng: -77.4041},
+  {lat: 39.0974, lng: -77.4895},
+  {lat: 38.8691, lng: -77.3740},
+  {lat: 38.8355, lng: -77.4247},
+  {lat: 38.8879, lng: -77.4282},
+  {lat: 38.8738, lng: -77.2742},
+  {lat: 38.8509, lng: -77.3512},
+  {lat: 38.9360, lng: -77.5530}
 ]
-}
